acaa9c21a86e29b59c06bba4826ad8bbca195779
annelise berthier <anne-lise.berthier@egis-group.com>
2024-07-18 12:29:52 +0200

    yolo first step the api to return an image still has to be coded

--- packagename/registry.py
+++ packagename/registry.py
@@ -3,6 +3,7 @@ import os
 from tensorflow import keras
 from tensorflow.keras.models import load_model
 from pathlib import Path
+from ultralytics import YOLO
 
 # Load the model
 def load_my_model() -> keras.Model:
@@ -12,8 +13,14 @@ def load_my_model() -> keras.Model:
     base_path = Path(__file__).parent.parent   #/models
     local_model_directory = base_path / "models/my_model.keras"
 
-    print(local_model_directory)
-
     model = load_model(local_model_directory)
+
+    return model
+
+def load_my_yolo_model():
+    base_path = Path(__file__).parent.parent   #/models
+    local_model_directory = base_path / "models/yolo_crack_segm_weights_30train.pt"
+
+    model = YOLO(local_model_directory)
 
     return model
